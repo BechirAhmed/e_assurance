b332ea222dbdda15b334fd29c178e665a7c7b739
Bechir Ahmed <BechirAhmed@users.noreply.github.com>
2022-05-18 18:07:08 +0000

    Update Assurance.php

--- app/Models/Assurance.php
+++ app/Models/Assurance.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Assurance extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'first_name',
+        'last_name',
+        'phone_number',
+        'nni',
+        'birthdate',
+        'n_permis',
+        'duration',
+        'vehicule_brand',
+        'vehicule_model',
+        'vehicule_type',
+        'vehicule_color',
+        'vehicule_chassis',
+        'vehicule_matricule',
+        'vehicule_first_use_date',
+        'vehicule_power',
+        'payment',
+    ];
 }
